d55ba9c52ab3e3f87ffd0196ae4d5f55193b6736
unknown <deepakb21000@gmail.com>
2026-05-24 04:10:53 +0530

    testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
  
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "@/Provider/Providers";
  
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -29,7 +21,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={` h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
     
